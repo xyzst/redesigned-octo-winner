bb429edc3dd0c3e3eef5a9b085cf91d6b905a050
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-02 17:06:22 -0500

    Update root component with BurgerBuilder component; add css file with custom css class to give the Layout component some presentational aspects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>teststetestetssets</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
