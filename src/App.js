67b0f6284a897a4437adda55dbed5d78f858914a
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-02 16:57:04 -0500

    Update root component to use Layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Layout>
+        <p>teststetestetssets</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
