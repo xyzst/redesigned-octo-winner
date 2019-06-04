47ae24d40fd390cebfdc4344aa6b048e4a44368f
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-03 21:48:03 -0500

    Add dynamic processing of Ingredient components using .map() method and Object methods

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import Ingredient from "./Ingredient/Ingredient";
 import classes from "./Burger.module.css";
 
 const Burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(x => {
+    return [...Array(props.ingredients[x])].map((_, i) => {
+      return <Ingredient key={x + i} type={x} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {transformedIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
