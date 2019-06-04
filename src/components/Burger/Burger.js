9608a6cb6a4348ed1fd939afdaf2b64ff70f40ff
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-04 12:47:55 -0500

    Add reducing function to flatten the transformedIngredients object into a single array of Ingredient components. Output a <p> element if there are no elements or ingredients to display

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import Ingredient from "./Ingredient/Ingredient";
 import classes from "./Burger.module.css";
 
 const Burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(x => {
-    return [...Array(props.ingredients[x])].map((_, i) => {
-      return <Ingredient key={x + i} type={x} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(x => {
+      return [...Array(props.ingredients[x])].map((_, i) => {
+        return <Ingredient key={x + i} type={x} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />
