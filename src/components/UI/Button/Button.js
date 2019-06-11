67b9bac230b9c26191647cca4f2c36a48bddbd74
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-10 21:37:09 -0500

    Fix styling issue with Button component -- add .join() method call on array of cCSS styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -7,7 +7,7 @@ import classes from "./button.module.css";
  */
 const Button = props => (
   <button
-    className={[classes.Button, classes[props.btnType]]}
+    className={[classes.Button, classes[props.btnType]].join(" ")}
     onClick={props.clicked}
   >
     {props.children}
