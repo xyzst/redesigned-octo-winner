cd630fb16e099b9ea16f479ed16c9933112186ff
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-17 19:48:23 -0500

    Add burger icon button. Add styling to Drawer Toggle component.

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,4 +1,12 @@
 import React from "react";
-const DrawerToggle = props => <div onClick={props.clicked}>Menu</div>;
+import classes from "./DrawerToggle.module.css";
+
+const DrawerToggle = props => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default DrawerToggle;
